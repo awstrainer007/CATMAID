96e2b8df6b5f32c4767fe0b792311464c97097e5
Chris Barnes <barnesc@janelia.hhmi.org>
2020-02-22 13:53:56 +0000

    Vagrant: Optional script for remainder of setup

--- scripts/vagrant/optional.sh
+++ scripts/vagrant/optional.sh
@@ -1,35 +1,50 @@
 #!/bin/bash
 set -e
+set -x
 
 DB_NAME=${DB_NAME:-"catmaid"}
 DB_USER=${DB_USER:-"catmaid_user"}
 DB_PASSWORD=${DB_PASSWORD:-"p4ssw0rd"}
-TIMEZONE=${TIMEZONE:-"America/New_York"}
+if [ -f "$HOME/timezone" ]; then
+    DEFAULT_TZ=$(cat $HOME/timezone)
+else
+    DEFAULT_TZ="America/New_York"
+fi
+TIMEZONE=${TIMEZONE:-$DEFAULT_TZ}
 
-mkdir ~/data
+mkdir -p ~/data
 
 # if it is not already,
 # prepend line to pg_hba.conf
 # and restart postgres
 HBA_LINE="local $DB_NAME $DB_USER md5"
 HBA_PATH="/etc/postgresql/11/main/pg_hba.conf"
-grep "$HBA_LINE" $HBA_PATH \
-    || sudo (echo "$HBA_LINE" && sudo cat $HBA_PATH) > $HBA_PATH \
-    && service postgresql restart
+LINE_EXISTS=""
+#LINE_EXISTS=$(sudo grep "$HBA_LINE" $HBA_PATH)
+echo "value is $LINE_EXISTS"
+if [ ! "$LINE_EXISTS" ]; then
+    echo "Allowing postgres to accept password connections"
+    echo "$HBA_LINE" > ~/tmp.txt
+    sudo cat $HBA_PATH >> ~/tmp.txt
+    sudo mv ~/tmp.txt $HBA_PATH
+    sudo service postgresql restart
+fi
 
 cd /CATMAID
 
+echo "Creating CATMAID user"
 scripts/createuser.sh $DB_NAME $DB_USER $DB_PASSWORD | sudo -u postgres psql
 
 cd django
 if [ ! -f projects/mysite/settings.py ]; then
+    echo "Configuring CATMAID"
     cp configuration.py.example configuration.py
 
     sed -i -e "s?^\(abs_catmaid_path = \).*?\1'/CATMAID'?g" configuration.py
     sed -i -e "s?^\(abs_virtualenv_python_library_path = \).*?\1'/home/vagrant/catmaid-env/lib/python3.6/site-packages'?g" configuration.py
 
     sed -i -e "s?^\(catmaid_database_name = \).*?\1'$DB_NAME'?g" configuration.py
-    sed -i -e "s?^\(catmaid_database_username = \).*?\1'$DB_USERNAME'?g" configuration.py
+    sed -i -e "s?^\(catmaid_database_username = \).*?\1'$DB_USER'?g" configuration.py
     sed -i -e "s?^\(catmaid_database_password = \).*?\1'$DB_PASSWORD'?g" configuration.py
 
     sed -i -e "s?^\(catmaid_writable_path = \).*?\1'/home/vagrant/data'?g" configuration.py
@@ -46,12 +61,11 @@ if [ ! -f projects/mysite/settings.py ]; then
     sed -i -e "s?^\(DEBUG = \).*?\1True?g" projects/mysite/settings.py
     # Enable static file serving without DEBUG = True
     echo "SERVE_STATIC = True" >> projects/mysite/settings.py
-    # TODO: Enable pipeline. Right now it doesn't seem to play well with Sauce Labs.
     #echo "PIPELINE['PIPELINE_ENABLED'] = False" >> projects/mysite/settings.py
     # Show full front-end errors by default
     echo "EXPAND_FRONTEND_ERRORS = True" >> projects/mysite/settings.py
 
-    cat projects/mysite/settings.py
+    # cat projects/mysite/settings.py
 fi
 
 cd projects
@@ -61,3 +75,4 @@ cd projects
 echo "\n\nCreating CATMAID superuser account\n"
 ./manage.py createsuperuser
 ./manage.py catmaid_insert_example_projects --user=1
+
